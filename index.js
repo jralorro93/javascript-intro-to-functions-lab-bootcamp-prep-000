5992c26a2e04e72fd9534af418624acbd043283c
Juan Alorro <jralorro93@gmail.com>
2018-12-13 22:21:50 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+var uppercase = "HELLO!";
+var lowercase = "hello!";
+var loveYa = "I love you, Grandma";
+
+function sayHiToGrandma(string) {
+  if (lowercase.toLowerCase === lowercase.toLowerCase)
+    return "I can\'t hear you!";
